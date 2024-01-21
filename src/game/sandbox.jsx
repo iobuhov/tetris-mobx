968a6a6272a193f41b35c7f2d4ae322f95f874ba
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-01-22 00:10:52 +0100

    chore: finish day 1, add first models

--- src/game/sandbox.jsx
+++ src/game/sandbox.jsx
@@ -1,6 +1,16 @@
 import { useMemo } from "react"
 import { GameVM } from "../modules/models/GameVM";
 import "./game.style.css";
+import { observer } from "mobx-react-lite";
+import cn from "classnames";
+
+const PointView = observer(({ point }) => <div key={Math.random()} className={cn("area-cell", { ["element"]: point.fill }, point.fill)} />)
+
+const GameArea = observer(({ renderer }) => (
+    <div tabIndex="0" className="game-area">
+        {renderer.points.map(point => <PointView key={point.id} point={point} />)}
+    </div>
+))
 
 export function Sandbox() {
     const game = useMemo(() => new GameVM(), []);
@@ -8,9 +18,7 @@ export function Sandbox() {
 
     return (
         <div className="game">
-            <div tabIndex="0" className="game-area">
-                {game.cells.map(cell => <div key={cell.id} className="area-cell" />)}
-            </div>
+            <GameArea renderer={game.renderer} />
             <div className="game-stats"></div>
         </div>
     )
